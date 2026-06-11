509766b2b955e585d87395f35b511c7fe0e9bc44
Abiddy <abidinouman@gmail.com>
2026-06-11 15:57:30 -0700

    Initial Commit

---
+++ platter/platterApp.swift
@@ -0,0 +1,32 @@
+//
+//  platterApp.swift
+//  platter
+//
+//  Created by Nouman Abidi on 6/11/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct platterApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ platterTests/platterTests.swift
@@ -0,0 +1,17 @@
+//
+//  platterTests.swift
+//  platterTests
+//
+//  Created by Nouman Abidi on 6/11/26.
+//
+
+import Testing
+@testable import platter
+
+struct platterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
